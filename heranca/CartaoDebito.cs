baa914c248783a28e9b238e8a07bb4f411d4290d
angelo559 <matheus.angelo0301@gmail.com>
2025-09-16 00:10:20 +0000

    feat: classe cartao

---
+++ heranca/CartaoDebito.cs
@@ -0,0 +1,24 @@
+using System.Data;
+
+public class CartaoDebito
+{
+    public CartaoDebito()
+    {
+        Bandeira = BandeiraCartao.Visa;
+    }
+
+    public string Numero { get; set; }
+    public BandeiraCartao Bandeira { get; set; }
+    public DateTime Vencimento { get; set; }
+    public string Portador { get; set; }
+    public string Cvv { get; set; }
+}
+
+public enum BandeiraCartao
+{
+    Visa,
+    Mastercard,
+    Amex,
+    Elo,
+
+}
